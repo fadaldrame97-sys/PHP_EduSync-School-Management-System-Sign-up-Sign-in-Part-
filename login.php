b37352247cb3cee630a9e182f47663e0ee2e9db0
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-04-21 11:48:54 +0100

    Interface login et sign up compplets

--- login.php
+++ login.php
@@ -1,5 +1,7 @@
 
- 
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -21,15 +23,11 @@
       <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                  Sign in to your account
+                  Log in to your account
               </h1>
               <form class="space-y-4 md:space-y-6" action="#" method="POST">
                   <div>
-                    <label for="nom" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Votre nom</label>
-                    <input type="text" name="nom" id="nome" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="fadal" required="">
-                    <label for="prenom" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Votre prenom</label>
-                    <input type="text" name="nome" id="nome" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Dramé" required="">
-                    
+                   
                     <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Votre email</label>
                       <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="">
                   </div>
@@ -60,6 +58,7 @@
 
 </body>
 </html>
+
 
 
     
