bfd32ae9cf210dd1567d1862172c55c8f3ce5eb1
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-04-23 16:26:27 +0100

    Afficher le nom de l'utilisateur connecté dans Dashboard

--- dashboard.php
+++ dashboard.php
@@ -14,10 +14,14 @@ if (!isset($_SESSION['us'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
+     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <link rel="stylesheet" href="style.css">
 </head>
-<body class="w-full">
-    <div class="w-70% w-75 bg-emerald-400 shadow-2xl rounded-lg">
-<h1>Bienvenue dans votre votre espace </h1>
+</head>
+<body class="flex justify-center items-center w-full h-100 text-center">
+    <div class="w-1/2 h-1/2 bg-emerald-400 shadow-2xl rounded-lg">
+<h1 class="flex justify-center text-center items-baseline-last pt-15 font-bold text-3xl">Bienvenue dans votre votre espace </h1>
+<p> <?php echo $_SESSION['us'];?></p>
 </div>
 </body>
 </html>
