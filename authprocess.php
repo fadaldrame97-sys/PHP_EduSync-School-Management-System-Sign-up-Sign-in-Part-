14cc775633d56311e59c509edbe2de2067e24ff8
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-04-23 16:01:23 +0100

    Affichage Dashboard Session_start

--- authprocess.php
+++ authprocess.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
 include_once'data.php';
+
+//Inscription
+
 if (isset($_POST["register"])){
 $nom=$_POST["nom"];
 $prenom=$_POST["prenom"];
@@ -13,6 +16,8 @@ $password=$_POST["password"];
 // print_r($_POST);
 // echo"<pre>";
 
+//vérification si l'email n'existe pas
+
 $sql="select* from users where email='$email'";
 $result=mysqli_query($connexion,$sql);
 
@@ -20,6 +25,8 @@ if(mysqli_num_rows($result)>0){
    header("Location: register.php?error=email_exists");
         exit();
 }
+
+//inserer les données dans database si l'email n'existe pas
 
 $sql="INSERT INTO users (nom,prnom ,email,passeword)
 values('$nom','$prenom','$email','$password')";
@@ -29,17 +36,21 @@ mysqli_query($connexion,$sql);
 header("location:login.php?success=registred");
 exit();
 
-    
+
+
+//récupérer login  
 
 }
 if (isset($_POST["login"])){
 $email=$_POST["email"];
 $password=$_POST["password"];
 
-
+//vérifé si l'email ou mots de passé correct ou déja existe
 
 $sql="select* from users where email='$email' AND  passeword='$password' ";
 $result=mysqli_query($connexion,$sql);
+
+//si il existe déja, on se redirige vers page dashboard
 
 if(mysqli_num_rows($result)==1){
 $_SESSION['us'] = $email;
@@ -47,6 +58,8 @@ $_SESSION['us'] = $email;
 header("location:dashboard.php");
 exit();
 }
+
+//Sinon email ou mots de passe incorrect
 else {
 header("location:login.php?error=invalid");
 exit();
