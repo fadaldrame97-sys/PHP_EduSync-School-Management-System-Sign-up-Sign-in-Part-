739b8ae71b6db1e5fb5489d105245858b3073914
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-04-23 16:14:24 +0100

    Message de bienvenue

--- dashboard.php
+++ dashboard.php
@@ -6,3 +6,18 @@ if (!isset($_SESSION['us'])) {
     exit();
 }
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+</head>
+<body class="w-full">
+    <div class="w-70% w-75 bg-emerald-400 shadow-2xl rounded-lg">
+<h1>Bienvenue dans votre votre espace </h1>
+</div>
+</body>
+</html>
