f249498f33bcb1234632cb67884daa9f71b38ed6
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-04-23 12:02:50 +0100

    Vérifier si l'email existe déja dans la base de données pendant la connexion

--- authprocess.php
+++ authprocess.php
@@ -7,13 +7,18 @@ $prenom=$_POST["prenom"];
 $email=$_POST["email"];
 $password=$_POST["password"];
 
-echo"Inscription réusssi";
+
 
 echo"<pre>";
 print_r($_POST);
 echo"<pre>";
 
-
+$sql="select* from users where email='$email'";
+$result=mysqli_query($connexion,$sql);
+if(mysqli_num_rows($result)>0){
+   header("Location: register.php?error=email_exists");
+        exit();
+}
 
 $sql="INSERT INTO users (nom,prnom ,email,passeword)
 values('$nom','$prenom','$email','$password')";
@@ -21,11 +26,7 @@ values('$nom','$prenom','$email','$password')";
 mysqli_query($connexion,$sql);
 
 
-$sql="select* from users where email='$email'";
-$result=mysqli_query($connexion,$sql);
-if(mysqli_num_rows($result)>0){
-    //email existe déja
-}
+
     
 
 }
@@ -35,8 +36,17 @@ $password=$_POST["password"];
 
 $_SESSION['us'] = $email;
 
+$sql="select* from users where email='$email'&& passeword='$password' ";
+$result=mysqli_query($connexion,$sql);
+
+if(mysqli_num_rows($result)>1){
 header("location:dashboard.php");
 exit();
+}
+
+
+
+
 
 
 
